b0d2b297543dbc7d0024f43dccc8f4d78a558914
brunda <brunda.r@bahwancybertek.com>
2022-12-21 11:50:00 +0530

    new

--- nest.js
+++ nest.js
@@ -1,1 +1,13 @@
-console.log('hello december');
+var http = require("http");
+
+
+
+http.createServer(function(request, response){
+
+    response.writeHead(200, {"COntent-type": "text/plain"});
+
+    response.write("Hello world");
+
+    response.end();
+
+}).listen(3000);
